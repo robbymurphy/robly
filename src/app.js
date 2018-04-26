688a91fb36483a9212078eef26b6a083614c44b8
Robert Murphy <robbymurphy@gmail.com>
2018-04-26 12:07:31 -0400

    400 error

--- src/app.js
+++ src/app.js
@@ -1,15 +1,54 @@
 const express = require('express');
-const slugGenerator = require('./slugGenerator');
+const { MongodHelper } = require('mongodb-prebuilt');
+const linkServiceFactory = require('./linkServiceFactory');
+const LinkService = require('./LinkService');
+const db = require('./db');
+
 const app = express();
 
 app.get('/shorten', (req, res) => {
-  res.json({
-    longUrl: req.query.longUrl,
-    shortUrl: `http://rob.ly/${slugGenerator()}`
-  });
+  const linkService = linkServiceFactory.createLinkService();
+  const expandedUrl = req.query.url;
+  if (!expandedUrl) {
+    // bad request
+    return res.status(400).json({ error: '"url" query parameter is required' })
+  }
+  const slug = req.query.slug;
+  linkService.shorten(expandedUrl, slug)
+    .then(link => res.json(link))
+    // internal server error
+    .catch(error => res.status(500).json({ error }));
+});
+
+app.get('/expand', (req, res) => {
+  const linkService = linkServiceFactory.createLinkService();
+  const shortUrl = req.query.shortUrl;
+  if (!shortUrl) {
+    // bad request
+    return res.status(400).json({ error: '"shortUrl" query parameter is required' })
+  }
+  linkService.expand(shortUrl)
+    .then(link => res.json(link))
+    // internal server error
+    .catch(error => res.status(500).json({ error }));
 });
 
 if (process.env.NODE_ENV !== 'test') {
+  // bootstrap if we aren't testing,
+  // if we're testing then we'll do custom bootstrapping
+  const mongodHelper = new MongodHelper(['--port', '27018']);
+  mongodHelper.run()
+    .then(started => {
+      console.log('mongodb started');
+      db.connect();
+    },
+    err => {
+      console.log('error starting mongodb', err);
+    });
+  linkServiceFactory.initialize({
+    LinkServiceClass: LinkService,
+    db
+  });
   app.listen(8081, () => console.log('app started'));
 }
 
--- test/app.test.js
+++ test/app.test.js
@@ -1,19 +1,120 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../src/app');
+const linkServiceFactory = require('../src/linkServiceFactory');
 const { expect } = chai;
 
 chai.use(chaiHttp);
 
+function createLinkServiceMock(shortenFn, expandFn) {
+  return class LinkServiceMock {
+    shorten() {
+      return shortenFn();
+    }
+    expand() {
+      return expandFn();
+    }
+  }
+}
+
 describe('app', function() {
-  it('should reply to /shorten GET request', done => {
-      chai.request(app)
-        .get('/shorten')
-        .then(res => {
-          expect(res.text).to.exist;
-          expect(res.status).to.equal(200);
-          done();
-        })
-        .catch(error => done(error));
+  it('should reply to valid /shorten GET request with 200', done => {
+    // Arrange
+    const LinkServiceMock = createLinkServiceMock(() => Promise.resolve({}));
+    linkServiceFactory.initialize({ LinkServiceClass: LinkServiceMock });
+
+    // Act
+    chai.request(app)
+      .get('/shorten?url=http://www.google.com')
+      .then(res => {
+        // Assert
+        expect(res.text).to.exist;
+        expect(res.status).to.equal(200);
+        done();
+      })
+      .catch(error => done(error));
+  });
+
+  it('should reply with 400 when missing url in /shorten GET request', done => {
+    // Arrange
+    const LinkServiceMock = createLinkServiceMock(() => Promise.resolve({}));
+    linkServiceFactory.initialize({ LinkServiceClass: LinkServiceMock });
+
+    // Act
+    chai.request(app)
+      .get('/shorten')
+      .then(res => {
+        // Assert
+        expect(res.status).to.equal(400);
+        done();
+      })
+      .catch(error => done(error));
+  });
+
+  it('should reply to invalid /shorten GET request with 500', done => {
+    // Arrange
+    const LinkServiceMock = createLinkServiceMock(() => Promise.reject(''));
+    linkServiceFactory.initialize({ LinkServiceClass: LinkServiceMock });
+
+    // Act
+    chai.request(app)
+      .get('/shorten?url=http://www.google.com')
+      .then(res => {
+        // Assert
+        expect(res.text).to.exist;
+        expect(res.status).to.equal(500);
+        done();
+      })
+      .catch(error => done(error));
+  });
+
+  it('should reply to valid /expand GET request with 200', done => {
+    // Arrange
+    const LinkServiceMock = createLinkServiceMock(null, () => Promise.resolve({}));
+    linkServiceFactory.initialize({ LinkServiceClass: LinkServiceMock });
+
+    // Act
+    chai.request(app)
+      .get('/expand?shortUrl=http://rob.ly/abcdefg')
+      .then(res => {
+        // Assert
+        expect(res.text).to.exist;
+        expect(res.status).to.equal(200);
+        done();
+      })
+      .catch(error => done(error));
+  });
+
+  it('should reply with 400 when missing shortUrl in /expand GET request', done => {
+    // Arrange
+    const LinkServiceMock = createLinkServiceMock(null, () => Promise.resolve({}));
+    linkServiceFactory.initialize({ LinkServiceClass: LinkServiceMock });
+
+    // Act
+    chai.request(app)
+      .get('/expand')
+      .then(res => {
+        // Assert
+        expect(res.status).to.equal(400);
+        done();
+      })
+      .catch(error => done(error));
+  });
+
+  it('should reply to invalid /expand GET request with 500', done => {
+    // Arrange
+    const LinkServiceMock = createLinkServiceMock(null, () => Promise.reject(''));
+    linkServiceFactory.initialize({ LinkServiceClass: LinkServiceMock });
+
+    // Act
+    chai.request(app)
+      .get('/expand?shortUrl=http://rob.ly/abcdefg')
+      .then(res => {
+        // Assert
+        expect(res.text).to.exist;
+        expect(res.status).to.equal(500);
+        done();
+      })
+      .catch(error => done(error));
   });
 });
