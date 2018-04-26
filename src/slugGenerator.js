8d06b2db5dcbde8495f839f392617668c677d6f3
Robert Murphy <robbymurphy@gmail.com>
2018-04-26 13:03:57 -0400

    update slugGenerator comments

--- src/slugGenerator.js
+++ src/slugGenerator.js
@@ -8,19 +8,24 @@ function fillCharArray(startChar, endChar) {
 }
 
 function randChar() {
+  // Generate a random character using a random number in the range of the allCharacters array length.
   return allCharacters[Math.floor(Math.random() * allCharacters.length)];
 }
 
-// map [A-Z][a-z][0-9] into lookup array
+// Map [A-Z][a-z][0-9] into lookup array.
 fillCharArray('a', 'z');
 fillCharArray('A', 'Z');
 fillCharArray('0', '9');
 
-// poor man's slug generation, we can be smarter about this!
-module.exports = function() {
+// Poor man's slug generation, we can be smarter about this!
+// This approach is a bit naive and may generate slug collisions
+// when the number of generated slugs increases.
+function slugGenerator() {
   let slug = '';
   for(let i = 0; i < slugLength; i++) {
     slug += randChar();
   }
   return slug;
 };
+
+module.exports = slugGenerator;
